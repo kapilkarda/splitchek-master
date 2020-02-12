dd14d905e7da9d68e23b14d14f057b66d49ebcda
Charmr <bmethai@hotmail.com>
2020-02-12 16:51:28 +0530

    url changes

--- appSettings.ts
+++ appSettings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
   //public static API_ENDPOINT='http://localhost:3000/';
   //public static API_ENDPOINT='http://54.71.18.74:4611/';
-  public static API_ENDPOINT='http://18.191.7.42:8080/';
+  public static API_ENDPOINT='http://18.216.34.21:8080/';
 }
